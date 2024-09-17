94b8e174c436ff1e42abadd14c47e06de7d37146
SHREYAS <shreyassonawane2572@gmail.com>
2024-09-17 12:02:39 +0530

    checking palindrome using recursion

---
+++ recursion/isPalindrome.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+bool isPalindrome(string &s,int start, int end){
+    if(start>=end){
+        return true;
+    }
+    if(s[start]!=s[end]){
+        return false;
+    }
+    return isPalindrome(s,start+1,end-1);
+}
+int main(){
+    string s = "racecar";
+    cout<<isPalindrome(s,0,s.size()-1);
+}
